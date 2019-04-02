a268d66150b0228523e23dca47480b3e6dffb05b
Sylvain Ferlac <sylvain@cirdan.net>
2019-04-02 21:07:57 +0200

    refactoring

--- src/Bowling.php
+++ src/Bowling.php
@@ -19,25 +19,9 @@ class Bowling
 
     public function pins_fallen(int $nb_pins)
     {
-        if ($this->strike) {
-            $nb_pins *= 2;
-            $this->nbFireSinceStrike ++;
-            if ($this->nbFireSinceStrike == 2) {
-                $this->nbFireSinceStrike = 0;
-                $this->strike = false;
-            }
-        }
-        if ($this->spare) {
-            $nb_pins *= 2;
-            $this->spare = false;
-        }
-        $this->total_score += $nb_pins;
-
-        if ($this->isStrike($nb_pins)) {
-            $this->strike = true;
-        } else if ($this->isSpare($this->last_pin, $nb_pins)) {
-            $this->spare = true;
-        }
+        $this->total_score += $this->computeScore($nb_pins);
+        
+        $this->checkForStrikeOrSpare($nb_pins);
 
         if ($this->spare || $this->strike) {
             $this->last_pin = $nb_pins/2;
@@ -59,5 +43,37 @@ class Bowling
     public function isStrike($first_pin)
     {
         return $first_pin === 10;
+    }
+
+    /**
+     * @param int $nb_pins
+     */
+    public function checkForStrikeOrSpare(int $nb_pins): void
+    {
+        if ($this->isStrike($nb_pins)) {
+            $this->strike = true;
+        } else if ($this->isSpare($this->last_pin, $nb_pins)) {
+            $this->spare = true;
+        }
+    }
+
+    /**
+     * @param int $nb_pins
+     * @return int
+     */
+    public function computeScore(int $nb_pins): int
+    {
+        if ($this->strike) {
+            $nb_pins *= 2;
+            $this->nbFireSinceStrike++;
+            if ($this->nbFireSinceStrike == 2) {
+                $this->nbFireSinceStrike = 0;
+                $this->strike = false;
+            }
+        } elseif ($this->spare) {
+            $nb_pins *= 2;
+            $this->spare = false;
+        }
+        return $nb_pins;
     }
 }
