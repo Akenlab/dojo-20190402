0b26f9690d512d8c911922aad95176b1dc43abc6
Sylvain Ferlac <sylvain@cirdan.net>
2019-04-02 20:14:41 +0200

    + [x] Strike and 1 and 2 score 16

--- src/Bowling.php
+++ src/Bowling.php
@@ -8,10 +8,23 @@ class Bowling
 {
 
     private $total_score = 0;
+    private $strike = false;
 
     public function pins_fallen(int $nb_pins)
     {
-        $this->total_score += $nb_pins;
+        if($this->strike)
+        {
+            $this->total_score += $nb_pins*2;
+        }
+        else
+        {
+            $this->total_score += $nb_pins;
+        }
+
+        if ($nb_pins === 10)
+        {
+            $this->strike = true;
+        }
     }
 
     public function total()
--- tests/BowlingTest.php
+++ tests/BowlingTest.php
@@ -22,4 +22,13 @@ class BowlingTest extends TestCase
         $this->assertSame(2,$game->total());
     }
 
+    public function test_strike_and_1_and_2_score_16()
+    {
+        $game=new Bowling();
+        $game->pins_fallen(10);
+        $game->pins_fallen(1);
+        $game->pins_fallen(2);
+        $this->assertSame(16,$game->total());
+
+    }
 }
