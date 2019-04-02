612ac3000414947f91035d5a98d5bef682792d0d
Sylvain Ferlac <sylvain@cirdan.net>
2019-04-02 19:46:49 +0200

    refactoring

--- tests/BowlingTest.php
+++ tests/BowlingTest.php
@@ -9,16 +9,16 @@ class BowlingTest extends TestCase
     public function test_2_gutters_score_0()
     {
         $game=new Bowling();
-        $game->pin_fallen(0);
-        $game->pin_fallen(0);
+        $game->pins_fallen(0);
+        $game->pins_fallen(0);
         $this->assertSame(0,$game->total());
     }
 
     public function test_1_gutter_and_2_pins_score_2()
     {
         $game=new Bowling();
-        $game->pin_fallen(0);
-        $game->pin_fallen(2);
+        $game->pins_fallen(0);
+        $game->pins_fallen(2);
         $this->assertSame(2,$game->total());
     }
 
